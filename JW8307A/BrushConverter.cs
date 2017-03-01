8a884919930bb36dd39f49163ef13a91d1e45866
Johnnyxt <Junlee1102@163.com>
2017-03-01 15:50:12 +0800

    The Third commit

---
+++ JW8307A/BrushConverter.cs
@@ -0,0 +1,88 @@
+﻿using JW8307A.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace JW8307A
+{
+    public class BrushConverterIl : IValueConverter
+    {
+        public static int i;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var ilLowLimit = Person.IlLowerThre[i];
+                var ilUpperLimit = Person.IlUpperThre[i];
+                i++;
+                float fValue;
+                if (value != null)
+                {
+                    fValue = System.Convert.ToSingle((string)value);
+                }
+                else
+                {
+                    return Brushes.Black;
+                }
+                if (fValue >= ilLowLimit && fValue < ilUpperLimit)
+                {
+                    return Brushes.Black;
+                }
+                return Brushes.Red;
+            }
+            catch (Exception ex)
+            {
+                // ignored
+            }
+
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class BrushConverterRl : IValueConverter
+    {
+        public static int i;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var rlLowLimit = Person.RlLowerThre[i];
+                var rlUpperLimit = Person.RlUpperThre[i];
+                i++;
+                float fValue;
+                if (value != null)
+                {
+                    fValue = System.Convert.ToSingle((string)value);
+                }
+                else
+                {
+                    return Brushes.Black;
+                }
+                if (fValue >= rlLowLimit && fValue < rlUpperLimit)
+                {
+                    return Brushes.Black;
+                }
+                return Brushes.Red;
+            }
+            catch (Exception ex)
+            {
+                // ignored
+            }
+
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ JW8307A/Models/TestItemModel.cs
@@ -0,0 +1,43 @@
+﻿using JW8307A.ViewModels;
+
+namespace JW8307A.Models
+{
+    internal class TestItemModel : NotificationObject
+    {
+        private string wave;
+
+        private string ilValue;
+
+        private string rlValue;
+
+        public string IlValue
+        {
+            get { return ilValue; }
+            set
+            {
+                ilValue = value;
+                RaisePropertyChanged("IlValue");
+            }
+        }
+
+        public string RlValue
+        {
+            get { return rlValue; }
+            set
+            {
+                rlValue = value;
+                RaisePropertyChanged("RlValue");
+            }
+        }
+
+        public string Wave
+        {
+            get { return wave; }
+            set
+            {
+                wave = value;
+                RaisePropertyChanged("Wave");
+            }
+        }
+    }
+}
